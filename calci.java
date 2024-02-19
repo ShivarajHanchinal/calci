9fc64984b634cb873ac94b89666c8bc4c978e092
ShivarajHanchinal <01fe22bca049@kletech.ac.in>
2024-02-19 14:32:54 +0530

    Code for calculator

--- calci.java
+++ calci.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class calci {
 
     void calci() {
-        double num1 = 30, num2 = 10;
+        double num1 = 10, num2 = 20;
 
         System.out.println("Num1=" + num1);
         System.out.println("Num2=" + num2);
@@ -19,6 +19,9 @@ class arithmatic {
     public static void main(String[] args) {
         calci z = new calci();
         z.calci();
-
+        int a;
+        extendedcalci b = new extendedcalci();
+        b.square(50);
+        b.cube(15);
     }
 }
