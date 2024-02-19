ad9e63bb379efcf09ef86dfaf5fe79233d5bddd6
ShivarajHanchinal <01fe22bca049@kletech.ac.in>
2024-02-19 14:33:24 +0530

    Code for functions on cube and square

---
+++ extendedcalci.java
@@ -0,0 +1,10 @@
+public class extendedcalci {
+    void square(int a) {
+        System.out.println("Square of number is : " + (a * a));
+    }
+
+    void cube(int a) {
+        System.out.println("Cube of number is : " + (a * a * a));
+    }
+
+}
